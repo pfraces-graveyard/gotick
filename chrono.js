a9b26fa8d67837d982ef5ce47d0d6cbf33b07ecf
Pau Fracés <pfraces@gmail.com>
2015-07-03 12:53:57 +0200

    Create chrono.js

---
+++ chrono.js
@@ -0,0 +1,41 @@
+angular.module('chrono', [])
+
+.factory('chrono', function () {
+  'use strict';
+  
+  return function () {
+    var accrued = 0;
+    var start = 0;
+    
+    var elapsedSince = function (startTime) {
+      return Date.now() - startTime;
+    };
+    
+    var startChrono = function () {
+      if (start) { return; }
+      start = Date.now();
+    };
+    
+    var stopChrono = function () {
+      if (!start) { return; }
+      accrued += elapsedSince(start);
+      start = 0;
+    };
+    
+    var resetChrono = function () {
+      accrued = 0;
+      start = 0;
+    };
+    
+    var elapsed = function () {
+      return accrued + elapsedSince(start)
+    };
+    
+    return {
+      start: startChrono,
+      stop: stopChrono,
+      reset: resetChrono,
+      elapsed: elapsed
+    };
+  };
+});
