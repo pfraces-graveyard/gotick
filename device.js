ff1d9d4c2b775a84eaf5943203b6b20dcff2421e
Pau Fracés <pfraces@gmail.com>
2015-07-04 23:08:10 +0200

    debugging menubutton

--- device.js
+++ device.js
@@ -3,9 +3,20 @@ angular.module('device', [])
 .factory('on', function ($document) {
   'use strict';
   
+  /*
   var on = function (eventName, eventListener) {
     $document.on('deviceready', function () {
       $document.on(eventName, eventListener);
+    });
+  };
+  */
+  var on = function (eventName, eventListener) {
+    $document.on('deviceready', function () {
+      alert('device is ready!');
+      $document.on(eventName, function () {
+        alert('event fired: ' + eventName);
+        eventListener();
+      });
     });
   };
   
