777dd503e71f9a88b747689e6c836611262f9ca9
Pau Fracés <pfraces@gmail.com>
2015-07-04 04:45:24 +0200

    Update gotick.js

--- gotick.js
+++ gotick.js
@@ -2,13 +2,35 @@ angular.module('gotick', ['chrono'])
 
 .controller('clock', function ($scope, $interval, chrono) {
   'use strict';
+  
+  var currentPlayer = function () {
+    return $scope[$scope.isBlackPlaying ? 'black' : 'white'];
+  };
+  
+  var remainingTimeFn = function (player) {
+    return function () {
+      var mainTime = $scope.settings.main * 60 * 1000;
+      var periodTime = $scope.settings.period * 1000;
+      var turnTime = player.periods ? periodTime : mainTime;
+      return turnTime - player.chrono.elapsed();
+    };
+  };
+  
+  var remainingPeriodsFn = function (player) {
+    return function () {
+      return $scope.settings.periods - player.periods;
+    };
+  };
 
   var initPlayer = function (name) {
-    $scope[name] = {
+    var player = $scope[name] = {
       elapsed: 0,
       periods: 0,
       chrono: chrono()
     };
+    
+    player.remainingTime = remainingTimeFn(player);
+    player.remainingPeriods = remainingPeriodsFn(player);
   };
   
   var init = function () {
@@ -19,37 +41,33 @@ angular.module('gotick', ['chrono'])
     initPlayer('white');
   };
   
-  var currentPlayer = function () {
-    return $scope[$scope.isBlackPlaying ? 'black' : 'white'];
-  };
-  
   var pollingChrono = null;
-  
-  var startPollingChrono = function () {
-    pollingChrono = $interval(function () {
-      var player = currentPlayer();
-      player.elapsed = player.chrono.elapsed();
-    }, 200);
-  };
   
   var stopPollingChrono = function () {
     $interval.cancel(pollingChrono);
   };
   
-  var zeroPad = function (str) {
-    str = '' + str;
-    return (str.length < 2 ? '0' : '') + str;
+  var pauseGame = function () {
+    $scope.playing = false;
+    currentPlayer().chrono.stop();
+    stopPollingChrono();
   };
   
-  var milisecondsToTime = function (miliseconds) {
-    var acc = miliseconds;
-    var ms = acc % 1000;
-    acc = (acc - ms) / 1000;
-    var secs = acc % 60;
-    acc = (acc - secs) / 60;
-    var mins = acc % 60;
-  
-    return zeroPad(mins) + ':' + zeroPad(secs);
+  var startPollingChrono = function () {
+    pollingChrono = $interval(function () {
+      var player = currentPlayer();
+      
+      if (player.remainingTime() <= 0) {
+        if (!player.remainingPeriods()) {
+          pauseGame();
+          return;
+        }
+        
+        player.periods += 1;
+        player.chrono.reset();
+        player.chrono.start();
+      }
+    }, 200);
   };
   
   $scope.settings = {
@@ -60,10 +78,23 @@ angular.module('gotick', ['chrono'])
   
   $scope.reset = init;
   
-  $scope.clockFmt = function (elapsed) {
-    var total = $scope.settings.main * 60 * 1000;
-    return milisecondsToTime(total - elapsed);
-  };
+  $scope.msFmt = (function () {
+    var zeroPad = function (str) {
+      str = '' + str;
+      return (str.length < 2 ? '0' : '') + str;
+    };
+  
+    return function (ms) {
+      var acc = ms;
+      var miliseconds = acc % 1000;
+      acc = (acc - miliseconds) / 1000;
+      var seconds = acc % 60;
+      acc = (acc - seconds) / 60;
+      var minutes = acc % 60;
+    
+      return zeroPad(minutes) + ':' + zeroPad(seconds);
+    };
+  })();
   
   $scope.play = function () {
     $scope.playing = true;
@@ -71,11 +102,7 @@ angular.module('gotick', ['chrono'])
     startPollingChrono();
   };
   
-  $scope.pause = function () {
-    $scope.playing = false;
-    currentPlayer().chrono.stop();
-    stopPollingChrono();
-  };
+  $scope.pause = pauseGame;
   
   $scope.switchPlayer = function () {
     if ($scope.playing) { currentPlayer().chrono.stop(); }
