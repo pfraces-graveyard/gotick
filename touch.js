e9179abd93cbf38e3e7ef52395791683e170f65a
Pau Fracés <pfraces@gmail.com>
2015-07-06 23:09:36 +0200

    testing touchstart

--- touch.js
+++ touch.js
@@ -1,12 +1,13 @@
 angular.module('touch', [])
 
-.directive('touchStart', function () {
+.directive('touchStart', function ($parse) {
   'use strict';
 
   var link = function (scope, element, attr) {
     var expr = $parse(attr.touchStart);
 
-    element.on('touchstart', function (event) {
+    element.on('touchstart mousedown', function (event) {
+      alert('touchstart or mousedown');
       scope.$apply(function () {
         expr(scope, { $event: event });
       });
