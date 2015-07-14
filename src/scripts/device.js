79f366c071f534e42f60c5b2c8a10c1fc1733367
Pau Fracés <pfraces@gmail.com>
2015-07-14 21:41:00 +0200

    testing /html/phonegap audio

--- src/scripts/device.js
+++ src/scripts/device.js
@@ -1,14 +1,23 @@
 angular.module('device', [])
 
-.factory('on', function ($document, $rootScope) {
+.factory('onReady', function ($document, $rootScope) {
+  'use strict';
+  
+  var onReady = function (eventListener) {
+    $document.on('deviceready', function () {
+      $rootScope.$apply(eventListener);
+    });
+  };
+  
+  return onReady;
+})
+
+.factory('on', function (onReady) {
   'use strict';
   
   var on = function (eventName, eventListener) {
-    $document.on('deviceready', function () {
-      $document.on(eventName, function () {
-        eventListener();
-        $rootScope.$apply();
-      });
+    onReady(function () {
+      $document.on(eventName, eventListener);
     });
   };
   
